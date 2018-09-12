824b86519de98513b6d10e49c5bc41ddd0843741
edbreeden <edbreeden@gmail.com>
2018-09-12 14:19:58 -0700

    Update update_locations.c

--- update_locations.c
+++ update_locations.c
@@ -3,28 +3,35 @@
 
 
 
-int *generate_random_list (unsigned sz)
+int *generate_random_list (unsigned sz, unsigned ext)
 {   
     int i;
-    int list[sz];
-    for (i = 0; i < sz; i++) {
-    	int r = (rand() % 1001);
+    int list[(sz * 3)];
+    for (i = 0; i < (sz * 3); i++) {
+    	int r = (rand() % (ext));
     	list[i] = r;
-        printf("val %d : %d\n", i, *(list + i));
     } 
     static int *xs;
     xs = list;
     return xs;
 }
+
 int main(int argc, char *argv[])
 {
-    int n,j;
+    int n,j,k,posext,vecext;
     n = atoi(argv[1]);
     srand(n);
-    int *rlist;
-    rlist = generate_random_list(n);
-    for (j = 0; j < n; j++) {
-    	printf("mval %d : %d\n", j, rlist[j]);
+    int *xlist;
+    posext = 1001;
+    vecext = 3;
+    xlist = generate_random_list(n, posext);
+    for (j = 0; j < (n; j++) {
+    	printf("xval %d : %d  ", j, xlist[j]);
     }
+    int *xvex;
+    //int *ylist;
+    xvex = generate_random_list(n, vecext);
+    for (k = 0; k < n; k++) {
+    	printf("mval %d : %d  ", k, xvex[k]);
     return 0;
 }
